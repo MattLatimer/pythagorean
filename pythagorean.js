08d483a53447e94402647b156f710b6b5d85c012
Matt <matt.latimer@gmail.com>
2017-09-07 18:20:47 +0000

    added pythagorean function code

--- pythagorean.js
+++ pythagorean.js
@@ -3,7 +3,11 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(coord) {
+  var x2 = Math.pow(coord.x, 2);
+  var y2 = Math.pow(coord.y, 2);
+  return Math.sqrt(x2 + y2);
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
